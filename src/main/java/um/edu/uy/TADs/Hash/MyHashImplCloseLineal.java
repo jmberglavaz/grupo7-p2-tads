ac3e0b1fd59a6d156f081bd320d2071c58227ec2
IagoMaSp <iagomasp2005@gmail.com>
2025-06-22 13:12:05 -0300

    Cambios locales

--- src/main/java/um/edu/uy/TADs/Hash/MyHashImplCloseLineal.java
+++ src/main/java/um/edu/uy/TADs/Hash/MyHashImplCloseLineal.java
@@ -3,7 +3,9 @@ package um.edu.uy.TADs.Hash;
 import um.edu.uy.Exceptions.ElementAlreadyExist;
 import um.edu.uy.Exceptions.ValueNoExist;
 
-public class MyHashImplCloseLineal<K,T> implements MyHash<K,T> {
+import java.util.Iterator;
+
+public class MyHashImplCloseLineal<K,T> implements MyHash<K,T>, Iterable<T> {
     private HashNode<K,T>[] table;
     private int size;         // Cantidad de elementos insertados
     private int capacity;     // Tamaño actual de la tabla (longitud del array)
@@ -17,14 +19,18 @@ public class MyHashImplCloseLineal<K,T> implements MyHash<K,T> {
     }
 
     @Override
+    public Iterator<T> iterator() {
+        return new MyHashIterator<>(this.table, this.deleteNode);
+    }
+
+    @Override
     public int size(){
         return this.size;
     }
 
     @Override
     public void insert(K clave, T data) throws ElementAlreadyExist {
-        // Verificar si necesitamos rehashing antes de insertar
-        if ((double) size / capacity >= maxFactorDeCarga) {
+        if ((double) size / capacity >= maxFactorDeCarga) { // Verificar si necesitamos rehashing antes de insertar
             incrementLength();
         }
 
@@ -41,7 +47,7 @@ public class MyHashImplCloseLineal<K,T> implements MyHash<K,T> {
 
         if (recorrido >= capacity) {
             // Esto no debería pasar con el control de factor de carga
-            System.err.println("Error: Tabla llena");
+            System.out.println("Error: Tabla llena");
             incrementLength();
             insert(clave, data);
             return;
@@ -73,12 +79,7 @@ public class MyHashImplCloseLineal<K,T> implements MyHash<K,T> {
         // Se duplica el tamaño y se encuentra el siguiente primo
         this.capacity = findNextPrime(this.capacity * 2);
 
-        //Developer function
-//        System.out.println("\nRealizando rehashing: tamano anterior = " + oldCapacity + ", tamano nuevo = " + this.capacity +
-//                ", el factor de carga era de = " + String.format("%.2f", (double) size / oldCapacity));
-
         this.table = new HashNode[this.capacity];
-        int oldSize = this.size;
         this.size = 0;
 
         // Se reinsertan todos los elementos
@@ -86,15 +87,9 @@ public class MyHashImplCloseLineal<K,T> implements MyHash<K,T> {
             if (node != null && node != deleteNode) {
                 try {
                     insert(node.getKey(), node.getData());
-                } catch (ElementAlreadyExist e) {
-                    // Esto no debería pasar durante rehashing
-                    e.printStackTrace();
-                }
+                } catch (ElementAlreadyExist ignored) {} // Esto no debería pasar durante rehashing
             }
         }
-
-        //Developer function
-//        System.out.println("Rehashing completado: " + oldSize + " elementos insertados\n");
     }
 
     @Override
@@ -157,78 +152,4 @@ public class MyHashImplCloseLineal<K,T> implements MyHash<K,T> {
         hash ^= (hash >>> 16);
         return Math.abs(hash) % capacity;
     }
-
-    // Devolper method
-    public void printStats() {
-        System.out.println("Hash Table Stats:");
-        System.out.println("  Tamano: " + size);
-        System.out.println("  Capacidad: " + capacity);
-        System.out.println("  Factor de Carga: " + String.format("%.2f", (double) size / capacity));
-
-        int maxProbes = 0;
-        int totalProbes = 0;
-        int usedSlots = 0;
-
-        for (int i = 0; i < capacity; i++) {
-            if (table[i] != null && table[i] != deleteNode) {
-                usedSlots++;
-                int probes = calculateProbes(table[i].getKey());
-                totalProbes += probes;
-                maxProbes = Math.max(maxProbes, probes);
-            }
-        }
-
-        if (usedSlots > 0) {
-            System.out.println("  Average probes: " + String.format("%.2f", (double) totalProbes / usedSlots));
-            System.out.println("  Max probes: " + maxProbes);
-        }
-        System.out.println("\n");
-    }
-
-
-    private int calculateProbes(K key) {
-        int index = hash(key);
-        int probes = 1;
-
-        while (table[index] != null && !table[index].getKey().equals(key)) {
-            index = (index + 1) % capacity;
-            probes++;
-        }
-
-        return probes;
-    }
-
-//// Iterator
-//    @Override
-//    public Iterator<HashNode<K, T>> iterator() {
-//        return new HashIterator();
-//    }
-//
-//    private class HashIterator implements Iterator<HashNode<K, T>> {
-//    private int indiceActual = 0;
-//    private int cantElementosRetornados = 0;
-//
-//    @Override
-//    public boolean hasNext() {
-//        return cantElementosRetornados < size; // xa checkear que haya un siguiente nodo, la cantidad de elementos recorridos tiene que ser menor al tamaño
-//    }
-//
-//    @Override
-//    public HashNode<K, T> next() {
-//        if (!hasNext()) {
-//            throw new NoSuchElementException("No hay siguiente elemento"); // uso exception de java porque me estaba dando error creando una yo mismo
-//        }
-//        // Busco el siguiente nodo válido:
-//        while (indiceActual < capacity) {
-//            HashNode<K, T> nodo = table[indiceActual];
-//            indiceActual++;
-//
-//            if (nodo != null && nodo != deleteNode) {
-//                cantElementosRetornados++;
-//                return nodo;
-//            }
-//        }
-//        throw new NoSuchElementException("No hay siguiente elemento");
-//    }
-//}
 }
--- src/main/java/um/edu/uy/TADs/Hash/MyHashIterator.java
+++ src/main/java/um/edu/uy/TADs/Hash/MyHashIterator.java
@@ -1,4 +1,38 @@
 package um.edu.uy.TADs.Hash;
 
-public class MyHashIterator {
+import java.util.Iterator;
+
+public class MyHashIterator<T> implements Iterator<T> {
+    private final HashNode<?, T>[] table;
+    private final HashNode<?, T> deleteNode;
+    private int currentIndex;
+
+    public MyHashIterator(HashNode<?, T>[] table, HashNode<?, T> deleteNode) {
+        this.table = table;
+        this.deleteNode = deleteNode;
+        this.currentIndex = 0;
+        moveToNextValid(); // Mover al primer elemento válido
+    }
+
+    public boolean hasNext() {
+        return currentIndex < table.length;
+    }
+
+    public T next() {
+        if (!hasNext()) {
+            throw new RuntimeException("No more elements");
+        }
+
+        T data = table[currentIndex].getData();
+        currentIndex++;
+        moveToNextValid();
+        return data;
+    }
+
+    private void moveToNextValid() {
+        while (currentIndex < table.length &&
+                (table[currentIndex] == null || table[currentIndex] == deleteNode)) {
+            currentIndex++;
+        }
+    }
 }
